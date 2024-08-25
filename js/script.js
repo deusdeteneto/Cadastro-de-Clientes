044a5948c1f6063558a9d15cc7d0b401518374f1
Torres-Neto <torresneto.tn@gmail.com>
2024-08-25 13:24:42 -0300

    closes #7 - Validação dos Campos do Formulário

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
 $("#inputCep").mask("00000-000");
+
+$(document).ready(function () {
+  function limpa_formulário_cep() {
+    // Limpa valores do formulário de cep.
+    $("#inputAddress").val("");
+    $("#inputDistrict").val("");
+    $("#inputCity").val("");
+    $("#inputState").val("");
+  }
+
+  //Quando o campo cep perde o foco.
+  $("#cep").blur(function () {
+    //Nova variável "cep" somente com dígitos.
+    var cep = $(this).val().replace(/\D/g, "");
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        //Preenche os campos com "..." enquanto consulta webservice.
+        $("#inputAddress").val("...");
+        $("#inputDistrict").val("...");
+        $("#inputCity").val("...");
+        $("#inputState").val("...");
+
+        //Consulta o webservice viacep.com.br/
+        $.getJSON(
+          "https://viacep.com.br/ws/" + cep + "/json/?callback=?",
+          function (dados) {
+            if (!("erro" in dados)) {
+              //Atualiza os campos com os valores da consulta.
+              $("#inputAddress").val(dados.logradouro);
+              $("#inputDistrict").val(dados.bairro);
+              $("#inputCity").val(dados.localidade);
+              $("#inputState").val(dados.uf);
+            } //end if.
+            else {
+              //CEP pesquisado não foi encontrado.
+              limpa_formulário_cep();
+              alert("CEP não encontrado.");
+            }
+          }
+        );
+      } //end if.
+      else {
+        //cep é inválido.
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+      }
+    } //end if.
+    else {
+      //cep sem valor, limpa formulário.
+      limpa_formulário_cep();
+    }
+  });
+});
